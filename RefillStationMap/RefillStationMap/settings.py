d447a42f20d35fb57ed448aa531790c71fa59a99
sujin-create <qsoojinq@naver.com>
2022-05-21 16:12:55 +0900

    ADD : mysql database RefillStationMap 적용

--- RefillStationMap/RefillStationMap/settings.py
+++ RefillStationMap/RefillStationMap/settings.py
@@ -89,8 +89,10 @@ WSGI_APPLICATION = 'RefillStationMap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RefillStationMap',
+        'USER': 'root',
+        "PASSWORD": env('DB_PW'),
     }
 }
 
