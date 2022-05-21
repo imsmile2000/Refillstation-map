d430b548b20cd4702b7bd6554ea38354f376abe5
sujin-create <qsoojinq@naver.com>
2022-05-21 16:52:57 +0900

    FEAT : 이름검색을 통한 filtering

--- RefillStationMap/Map/views.py
+++ RefillStationMap/Map/views.py
@@ -9,37 +9,37 @@ import json
 # Create your views here.
 def store(request):
 
-    # store 카테고리별로 분류해서 json으로 store.html에 넘기기 => res_data 생성할 때 필터링해서 특정 데이터만 넣어
-    store_del = Store.objects.all()
-    print("delete ==================>",store_del)
-    store_del.delete()
-    print(Store.objects.all())
-
-    store = Store()
+    # # store 카테고리별로 분류해서 json으로 store.html에 넘기기 => res_data 생성할 때 필터링해서 특정 데이터만 넣어
+    # store_del = Store.objects.all()
+    # print("delete ==================>",store_del)
+    # store_del.delete()
+    # print(Store.objects.all())
 
     stores = st()
+    print(stores)
 
     list=[]
 
     for i in range(0,len(stores)):
-        print([stores['상점명'][i],stores['길찾기'][i],stores['대분류'][i]])
+        # print([stores['상점명'][i],stores['길찾기'][i],stores['대분류'][i]])
         c_list = stores['대분류'][i].split(',')
-        print(c_list)
+        # print(c_list)
         category = json.dumps(c_list)
         list.append([stores['상점명'][i],stores['길찾기'][i],stores['lati'][i],stores['longi'][i],category])
 
     for refillstation in list:
-        if Store.objects.filter(latitude=refillstation[2]):
-            if Store.objects.filter(longitude=refillstation[3]):
+        if Store.objects.filter(name=refillstation[0]):
                 print("이미존재")
         else:
+            store = Store()
+            print("save>>>>>>>>>>>>>>>",refillstation)
             store.name  = refillstation[0]
             store.site =  refillstation[1]
             store.latitude = refillstation[2]
             store.longitude = refillstation[3]
             store.category = refillstation[4]
             # store.category = refillstation[5]
-            store.save()
+            # store.save()
 
     return render(request,'store.html')
 
@@ -51,31 +51,31 @@ def map(request):
         check = request.POST['radiocheck']
 
         # 1. 사용자가 check한 카테고리를 필터링. => 식품 화장품 생활용품중 하나
-        input = check
-        res_data=[]
-        stores = Store.objects.all()
-        print("all:=============> ",stores)
-        jsonDec = json.decoder.JSONDecoder()
-        for store in stores:
-            category = jsonDec.decode(store.category)
-            print(category)
-            if input in category:
-                if store in stores:
-                    print("=================store:",store)
-                    res_data.append(store)
-                    print(res_data)
+        if check:
+            input = check
+            res_data=[]
+            stores = Store.objects.all()
+            print("all:=============> ",stores)
+            jsonDec = json.decoder.JSONDecoder()
+            for store in stores:
+                category = jsonDec.decode(store.category)
+                print(category)
+                if input in category:
+                    if store in stores:
+                        print("=================store:",store)
+                        res_data.append(store)
+                        # print(res_data)
 
         # 2. 사용자가 입력한 값으로 필터링 -> 세부필터링( 주 카테고리 중에서 keyword에 해당하는 것만 출력)
-        # input2 = keyword #서브카테고리 키워드
-        # filter_stores = Store.objects.filter(category=check)
-        # jsonDec = json.decoder.JSONDecoder()
-        # for store in filter_stores:
-        #     sub_category = jsonDec.decode(store.sub_category)
-        #     print(sub_category)
-        #     if input2 in sub_category:
-        #         if store in stores:
-        #             res_data.append(store)
-        #             print(res_data)
+        # 이때, 세부카테고리를 아직 정하지 않았기에 name으로 filtering
+        if keyword:
+            input2 = keyword #서브카테고리 키워드
+            filter_stores = Store.objects.filter(name__contains = input2)
+            # jsonDec = json.decoder.JSONDecoder()
+            for store in filter_stores:
+                # sub_category = jsonDec.decode(store.sub_category)
+                res_data.append(store)
+                print(res_data)
         #서브카테고리 데이터 추가 필요.
         
         stores_js = serializers.serialize("json", res_data)
