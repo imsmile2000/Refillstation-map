d1463f8d3fdd1e3dac9c09b80a71a65c1c325485
sujin-create <qsoojinq@naver.com>
2022-09-12 14:32:44 +0900

    Fix : merge conflict 해결

--- RefillStationMap/main/views.py
+++ RefillStationMap/main/views.py
@@ -14,13 +14,9 @@ def products(request):
     return render(request,'products.html')
 
 def store(request):
-    
-<<<<<<< HEAD
+
     return render(request,'store.html')
 
 def components(request):
 
     return render(request,'app-components.html')
-=======
-    return render(request,'store.html')
->>>>>>> 8258be236a64babd257eb4de277d4ffaafddfce9
