0bad55e81a9f4bccf34c68c73e5e784bb6934fb4
imsmile2000 <imsmile2000@naver.com>
2022-09-15 03:23:41 +0900

    09.15

--- RefillStationMap/Item/item_save.py
+++ RefillStationMap/Item/item_save.py
@@ -4,12 +4,13 @@ import re
 
 def item_save():
 
-    df = pd.read_csv('/Users/baeksujin/Desktop/RefillStationMap/RefillStation_Map/RefillStationMap/Item/item.csv')
+    df = pd.read_csv('/Users/baeksujin/Desktop/RefillStationMap/RefillStation_Map/RefillStationMap/Item/item2.csv')
 
     item = Item
     for i in range(0,len(df)):
         price_temp =  df['price'][i]
         price = re.sub(",","",price_temp)
+        price = re.sub("원","",price)
         
 
         item(image = df['image'][i], price = price, name=df['name'][i],detail = df['detail'][i]).save()
